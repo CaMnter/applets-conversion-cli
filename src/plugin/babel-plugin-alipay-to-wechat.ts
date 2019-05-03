4d5d3e92e423aec15aade78f6944a4442d60435b
CaMnter <yuanyu.camnter@gmail.com>
2019-05-03 20:54:15 +0800

    「Update」BabelPluginAlipayToWechat

--- src/plugin/babel-plugin-alipay-to-wechat.ts
+++ src/plugin/babel-plugin-alipay-to-wechat.ts
@@ -2,14 +2,43 @@
  * Created by：CaMnter
  */
 
-import { BabelPluginBaseApplet } from "./base/babel-plugin-base-applet";
+import { AppletType, BabelPluginBaseApplet } from "./base/babel-plugin-base-applet";
 
 export class BabelPluginAlipayToWechat extends BabelPluginBaseApplet {
 
   public createPlugin(): Function {
-    return function () {
-      // TODO
+    const _identifierHook = this.identifierHook.bind(this);
+    const _memberExpressionHook = this.memberExpressionHook.bind(this);
+    return function (): object {
+      return {
+        name: 'babel plugin alipay to wechat',
+        visitor: {
+
+          /**
+           * my
+           *
+           * @param path
+           * @param appletType
+           * @constructor
+           */
+          Identifier(path: { get: Function, scope: { hasBinding: Function }, isReferencedIdentifier: Function, replaceWith: Function }) {
+            _identifierHook(path, AppletType.my)
+          },
+
+          /**
+           * my['request']
+           *
+           * my.request
+           * my[functionName]
+           *
+           * @param path path
+           * @constructor constructor
+           */
+          MemberExpression(path: { get: Function }) {
+            _memberExpressionHook(path, AppletType.my);
+          },
+        }
+      };
     };
   }
-
 }
