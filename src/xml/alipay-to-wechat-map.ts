9932c6b6a611902f28b9a4b5ca4eb682899f2489
CaMnter <yuanyu.camnter@gmail.com>
2019-05-05 22:35:05 +0800

    「Add」xml map

---
+++ src/xml/alipay-to-wechat-map.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 CaMnter yuanyu.camnter@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Created by：CaMnter
+ */
+
+export const alipayToWechatXmlMap = {
+  'a:if': 'wx:if',
+  'a:elif': 'wx:elif',
+  'a:else': 'wx:else',
+  'a:for': 'wx:for',
+  // 没有 a:for-items
+  'a:for-index': 'wx:for-index',
+  'a:for-item': 'wx:for-item',
+  'a:key': 'a:key',
+  'onTap': 'onTap',
+  'catchTap': 'catchTap',
+  'onInput': 'bindinput',
+  'onChange': 'bindchange',
+  'onFocus': 'bindfocus',
+  'onSubmit': 'bindsubmit',
+
+  // TODO
+};
---
+++ src/xml/wechat-to-alipay-map.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 CaMnter yuanyu.camnter@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Created by：CaMnter
+ */
+
+export const wechatToAlipayXmlMap = {
+  'wx:if': 'a:if',
+  'wx:elif': 'a:elif',
+  'wx:else': 'a:else',
+  'wx:for': 'a:for',
+  'wx:for-items': 'a:for',
+  'wx:for-index': 'a:for-index',
+  'wx:for-item': 'a:for-item',
+  'wx:key': 'a:key',
+  'bindtap': 'onTap',
+  'catchtap': 'catchTap',
+  'bindinput': 'onInput',
+  'bindchange': 'onChange',
+  'bindfocus': 'onFocus',
+  'bindsubmit': 'onSubmit',
+
+  // TODO
+};
