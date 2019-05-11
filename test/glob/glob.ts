984b55d790af4d992390021b31c2f7f0e4306ed6
CaMnter <yuanyu.camnter@gmail.com>
2019-05-11 23:27:18 +0800

    「Add」glob「unit test」

---
+++ test/glob/glob.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2019 CaMnter yuanyu.camnter@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Created by：CaMnter
+ */
+
+import * as glob from 'glob';
+import { expect } from 'chai';
+
+describe('「glob」', function () {
+
+  it('glob sync', () => {
+    const result = glob.sync("!(node_modules)/**/*.{js,ts,json}");
+    const expectResult = Array.isArray(result);
+    expect(expectResult).to.equal(true);
+  });
+
+});
