472eab1e44a4e272c83c5f2ef20510d7c1503ff5
CaMnter <yuanyu.camnter@gmail.com>
2019-05-26 17:39:05 +0800

    「Update」babel-plugin-wechat-to-alipay

--- src/lib/babel-plugin/babel-plugin-wechat-to-alipay.ts
+++ src/lib/babel-plugin/babel-plugin-wechat-to-alipay.ts
@@ -28,64 +28,11 @@ class BabelPluginWechatToAlipay extends BabelPluginBaseApplet {
   }
 
   getTarget(): AppletType {
-    return AppletType.my;
-  }
-
-  getExpect(): AppletType {
     return AppletType.wx;
   }
 
-  public createPlugin(): Function {
-    const _identifierHook = this.identifierHook.bind(this);
-    const _callExpressionHook = this.callExpressionHook.bind(this);
-    const _memberExpressionHook = this.memberExpressionHook.bind(this);
-    return function (): object {
-      return {
-        name: 'babel babel-plugin wechat to alpay',
-        visitor: {
-
-          /**
-           * wx
-           *
-           * @param path { get: Function, scope: { hasBinding: Function }, isReferencedIdentifier: Function, replaceWith: Function }
-           * @constructor constructor
-           */
-          Identifier(path: {
-            get: Function,
-            scope: { hasBinding: Function },
-            isReferencedIdentifier: Function,
-            replaceWith: Function
-          }) {
-            _identifierHook(path, AppletType.wx)
-          },
-
-          /**
-           * wx.request({ url: 'https://www.camnter.com' })
-           * wx['request']({ url: 'https://www.camnter.com' })
-           * wx[functionName]({ url: 'https://www.camnter.com' })
-           *
-           * @param path { get: Function }
-           * @constructor constructor
-           */
-          CallExpression(path: { get: Function }) {
-            _callExpressionHook(path, AppletType.wx);
-          },
-
-          /**
-           * wx['request']
-           *
-           * wx.request
-           * wx[functionName]
-           *
-           * @param path { get: Function }
-           * @constructor constructor
-           */
-          MemberExpression(path: { get: Function }) {
-            _memberExpressionHook(path, AppletType.wx);
-          },
-        }
-      };
-    };
+  getExpect(): AppletType {
+    return AppletType.my;
   }
 
 }
